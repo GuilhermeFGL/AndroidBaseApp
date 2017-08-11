65ad5688a37ccbaf69aac434dc324992c6eead8d
ECXCARD\guilherme.lima <montante1!>
2017-08-11 10:42:54 -0300

    tests

--- app/src/androidTest/java/com/example/guilherme/firebasedatabse/config/FirebaseTest.java
+++ app/src/androidTest/java/com/example/guilherme/firebasedatabse/config/FirebaseTest.java
@@ -7,27 +7,28 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(AndroidJUnit4.class)
 public class FirebaseTest {
 
     @Test
     public void getFirebaseDatabase_verifyConnection() throws Exception {
-        assertNotEquals(null, Firebase.getFirebaseDatabase());
+        assertNotNull(Firebase.getFirebaseDatabase());
     }
 
     @Test
     public void getFirebaseAuth_verifyConnection() throws Exception {
-        assertNotEquals(null, Firebase.getFirebaseAuth());
+        assertNotNull(Firebase.getFirebaseAuth());
     }
 
     @Test
     public void getStorageReference_verifyConnection() throws Exception {
-        assertNotEquals(null, Firebase.getStorageReference("6VeQdKsEvFOwNTRXWvHlxBn7BFB2.jpg"));
+        assertNotNull(Firebase.getStorageReference("6VeQdKsEvFOwNTRXWvHlxBn7BFB2.jpg"));
     }
 
     @Test
     public void isUserLoggedIn_verifyConnection() throws Exception {
-        assertEquals(false, Firebase.isUserLoggedIn());
+        assertNotNull(Firebase.isUserLoggedIn());
     }
 }
--- app/src/androidTest/java/com/example/guilherme/firebasedatabse/helper/LocalPreferencesTest.java
+++ app/src/androidTest/java/com/example/guilherme/firebasedatabse/helper/LocalPreferencesTest.java
@@ -6,12 +6,16 @@ import android.support.test.runner.AndroidJUnit4;
 
 import com.example.guilherme.firebasedatabse.config.Constants;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.HashMap;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class LocalPreferencesTest {
@@ -19,14 +23,20 @@ public class LocalPreferencesTest {
     private Context appContext = InstrumentationRegistry.getTargetContext();
     private LocalPreferences preferences = new LocalPreferences(appContext);
 
+    @Before
+    public void setUser() {
+        preferences.saveUser("Nome", "email");
+        preferences.saveAvatar("URL Avatar");
+    }
+
     @Test
     public void getNotificationPreferences_assertDefaultValues() throws Exception {
-        assertEquals(true, preferences.getNotificationPreferences());
+        assertTrue(preferences.getNotificationPreferences());
     }
 
     @Test
     public void getVibratePreferences_assertDefaultValues() throws Exception {
-        assertEquals(true, preferences.getVibratePreferences());
+        assertTrue(preferences.getVibratePreferences());
     }
 
     @Test
@@ -36,9 +46,6 @@ public class LocalPreferencesTest {
 
     @Test
     public void saveUser_saveAvatar_verifySavedValues() throws Exception {
-        preferences.saveUser("Nome", "email");
-        preferences.saveAvatar("URL Avatar");
-
         HashMap<String, String> user = preferences.getUser();
         assertEquals("Nome", user.get(Constants.USER_NAME));
         assertEquals("email", user.get(Constants.USER_TOKEN));
@@ -50,8 +57,8 @@ public class LocalPreferencesTest {
         preferences.logoutUser();
 
         HashMap<String, String> user = preferences.getUser();
-        assertEquals(null, user.get(Constants.USER_NAME));
-        assertEquals(null, user.get(Constants.USER_TOKEN));
-        assertEquals(null, user.get(Constants.USER_AVATAR));
+        assertNull(user.get(Constants.USER_NAME));
+        assertNull(user.get(Constants.USER_TOKEN));
+        assertNull(user.get(Constants.USER_AVATAR));
     }
 }
